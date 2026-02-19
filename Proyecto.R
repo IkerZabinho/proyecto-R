53a3655cbf55ae5b02d1f49f338128cb1ec4cfa6
Iker Zabala <tuemail@ejemplo.com>
2026-02-19 18:24:15 +0100

    a

--- Proyecto.R
+++ Proyecto.R
@@ -14,7 +14,7 @@ merged
 # in both datasets), we filter it just in case there are missing values or any
 # false values that accidentally got in
 filtered <- merged %>% 
-  filter(Year>=2010 & Year<=2015) %>%
+  filter(Year>=2010 & Year<=2015)
   
 
 filtered
