8bcb8026c05850c547ffba00059458d763f4079b
Iker Zabala <iker.z.e@opendeusto.es>
2026-05-10 18:31:38 +0200

    Aldaketak

--- Backup.R
+++ Backup.R
@@ -38,3 +38,58 @@ fviz_ca_biplot(res.ca, repel = TRUE,
 
 fviz_ca_row(res.ca, col.row = "contrib", 
             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"))
+
+
+
+
+
+
+
+
+
+
+###### PCA# #######
+# Selecting 7 numeric variables and a categorical one (Status)
+df_pca_raw <- merged %>%
+  select(LifeExpectancyMen, AdultMortalityMen, Schooling, 
+         GDPCurrentUSD, Alcohol, BMI, HIV, Status) %>%
+  drop_na()
+
+# Column 8, in this case Status, is set as a supplementary qualitative variable
+res.pca <- PCA(df_pca_raw, quali.sup = 8, scale.unit = TRUE, graph = FALSE)
+
+#We want to see if the components are optimal
+print("--- Eigenvalues ---")
+print(res.pca$eig)
+fviz_eig(res.pca, addlabels = TRUE, main = "Scree Plot")
+
+print("--- Variable Correlations ---")
+print(res.pca$var$coord)
+
+# Dim 1 represents Social Development (Strong link to Schooling: 0.86)
+# Dim 2 represents Economy and Mortality (Strong link to GDP: 0.64)
+
+# Correlation Circle visualization
+fviz_pca_var(res.pca, col.var = "contrib", 
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE, title = "Variables - PCA") #Using this, we see the correlation among variables in a visual way
+
+# CATEGORICAL PROJECTION (Requirement D)
+# Projecting 'Status' (Developed vs Developing)
+# Developed countries (TRUE) cluster on the right side (High Dim 1)
+fviz_pca_ind(res.pca,
+             geom.ind = "point", 
+             col.ind = df_pca_raw$Status, 
+             palette = c("#FC4E07", "#00AFBB"),
+             addEllipses = TRUE,
+             legend.title = "Status",
+             title = "PCA: Countries by Development Status")
+
+# CONTRIBUTIONS AND QUALITY
+# Check which variables contribute most to Dim 1
+print("Variable Contributions to Dim 1:")
+sort(res.pca$var$contrib[,1], decreasing = T)
+
+# Quality of representation (cos2)
+print("Quality of representation (cos2):")
+print(res.pca$var$cos2)
--- Deliverable3.R
+++ Deliverable3.R
@@ -28,52 +28,52 @@ new_names <- c("Country",                         "Year",                       
 #Apply the new names
 merged <- setNames(merged, new_names)
 
-
 ###### PCA# #######
+df_grouped <- merged %>%
+  group_by(Country, Status) %>%
+  summarise(across(where(is.numeric), function(x) mean(x, na.rm = TRUE)), .groups = "drop")
+
 # Selecting 7 numeric variables and a categorical one (Status)
-df_pca_raw <- merged %>%
-  select(LifeExpectancyMen, AdultMortalityMen, Schooling, 
+df_pca_data <- df_grouped %>% 
+  select(Country, LifeExpectancyMen, AdultMortalityMen, Schooling, 
          GDPCurrentUSD, Alcohol, BMI, HIV, Status) %>%
   drop_na()
 
+df_final <- as.data.frame(df_pca_data)
+rownames(df_final) <- df_final$Country
+df_final <- df_final %>% select(-Country)
+
 # Column 8, in this case Status, is set as a supplementary qualitative variable
-res.pca <- PCA(df_pca_raw, quali.sup = 8, scale.unit = TRUE, graph = FALSE)
+res.pca <- PCA(df_final, quali.sup = 8, scale.unit = TRUE, graph = FALSE)
+
+# CONTRIBUTIONS AND QUALITY
 
 #We want to see if the components are optimal
-print("--- Eigenvalues ---")
-print(res.pca$eig)
-fviz_eig(res.pca, addlabels = TRUE, main = "Scree Plot")
+print("--- Autovalores (Eigenvalues) ---")
+print(res.pca$eig) 
 
-print("--- Variable Correlations ---")
-print(res.pca$var$coord)
+# Check which variables contribute most to Dim 1
+print("--- Contribuciones a la Dimensión 1 ---")
+sort(res.pca$var$contrib[,1], decreasing = TRUE) 
 
-# Dim 1 represents Social Development (Strong link to Schooling: 0.86)
-# Dim 2 represents Economy and Mortality (Strong link to GDP: 0.64)
+# Quality of representation (cos2)
+print("--- Calidad de Representación (cos2) ---")
+print(res.pca$var$cos2)
+
+fviz_eig(res.pca, addlabels = TRUE, ylim = c(0, 50))
 
 # Correlation Circle visualization
 fviz_pca_var(res.pca, col.var = "contrib", 
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-             repel = TRUE, title = "Variables - PCA") #Using this, we see the correlation among variables in a visual way
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), repel = TRUE)
+#Using this, we see the correlation among variables in a visual way
+
 
 # CATEGORICAL PROJECTION (Requirement D)
 # Projecting 'Status' (Developed vs Developing)
-# Developed countries (TRUE) cluster on the right side (High Dim 1)
-fviz_pca_ind(res.pca,
-             geom.ind = "point", 
-             col.ind = df_pca_raw$Status, 
-             palette = c("#FC4E07", "#00AFBB"),
-             addEllipses = TRUE,
-             legend.title = "Status",
-             title = "PCA: Countries by Development Status")
+# Developed countries (blue) cluster on the right side (High Dim 1)
+fviz_pca_ind(res.pca, habillage = 8, addEllipses = TRUE, repel = TRUE, label = "ind")
+res.pca$var$contrib[,1]
 
-# CONTRIBUTIONS AND QUALITY
-# Check which variables contribute most to Dim 1
-print("Variable Contributions to Dim 1:")
-sort(res.pca$var$contrib[,1], decreasing = T)
-
-# Quality of representation (cos2)
-print("Quality of representation (cos2):")
-print(res.pca$var$cos2)
 
 ##############
 #### CA ######
@@ -167,30 +167,9 @@ cluster_interpretation <- df_pca_raw %>%
 
 print(cluster_interpretation)
 
-#--------- hemengo hau etzeiat berrezkue deken bñ basikamente herrialdezka eiteko pca filaka einberrien
 
 
-df_grouped <- merged %>%
-  group_by(Country, Status) %>%
-  summarise(across(where(is.numeric), function(x) mean(x, na.rm = TRUE)), .groups = "drop")
-
-df_pca_data <- df_grouped %>% 
-  select(Country, LifeExpectancyMen, AdultMortalityMen, Schooling, 
-         GDPCurrentUSD, Alcohol, BMI, HIV, Status) %>%
-  drop_na()
-
-df_final <- as.data.frame(df_pca_data)
-rownames(df_final) <- df_final$Country
-df_final <- df_final %>% select(-Country)
-
-res.pca <- PCA(df_final, quali.sup = 8, scale.unit = TRUE, graph = FALSE)
-
-fviz_eig(res.pca, addlabels = TRUE, ylim = c(0, 50))
-
-fviz_pca_var(res.pca, col.var = "contrib", 
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), repel = TRUE)
-
-fviz_pca_ind(res.pca, habillage = 8, addEllipses = TRUE, repel = TRUE, label = "ind")
+##CLUSTERING
 
 pca_coords <- res.pca$ind$coord[, 1:2]
 
